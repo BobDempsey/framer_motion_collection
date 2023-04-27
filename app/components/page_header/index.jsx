c852ec06e133a73f3c917a451f3bae040fdb763b
Bob Dempsey <bobdempsey83@gmail.com>
2023-04-26 22:23:12 -0400

    adds a few todos to the main page header

--- app/components/page_header/index.jsx
+++ app/components/page_header/index.jsx
@@ -6,6 +6,14 @@ const PageHeader = () => {
         A collection of fun UI animations and interactions using Framer Motion
         and React
       </h3>
+      <pre>
+        add a layout animation using framer when the down caret is clicked to
+        show the description
+      </pre>
+      <pre>
+        add a modal, description, or footer describing this app `built with x,
+        hosted on x`
+      </pre>
     </section>
   );
 };
